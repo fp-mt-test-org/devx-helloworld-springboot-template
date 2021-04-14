d1604df84c2d3fe88bd064dd71132ed21178b8d3
Matthew Thomas <matthewthomas@flexport.com>
2021-04-14 11:37:04 -0700

    Remove unncessary files from template.

--- {{cookiecutter.component_id}}/.buildkite/prepare_cd_env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$MANUAL_DEPLOY" = "false" ]; then
-    export QUEUE="fpos-staging-nonprod-deploy"
-    export DEPLOY_ENV="staging"
-else
-    export QUEUE="fpos-dev-1-nonprod-deploy"
-    export DEPLOY_ENV="development"
-fi
