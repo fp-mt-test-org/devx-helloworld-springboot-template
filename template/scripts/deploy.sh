4bf9fb783bfd57eb458cf07b08839eeda92e6ce5
Matthew Thomas <matthewthomas@flexport.com>
2021-07-09 13:40:15 -0700

    fix: Values

--- template/scripts/deploy.sh
+++ template/scripts/deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 ./scripts/setup-kubectl-for-gcloud.sh
 
-helm upgrade --install --atomic springtest src/main/helm -n default -f src/main/helm/values-"${ENV}.yaml" --set config.image.tag="${TAG}"
+helm upgrade --install --atomic springtest src/main/helm -n default -f src/main/helm/values.yaml --set config.image.tag="${TAG}"
