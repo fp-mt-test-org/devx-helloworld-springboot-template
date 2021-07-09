1acfde2cd8014064af4066982e9ea8a69b13deb1
Matthew Thomas <matthewthomas@flexport.com>
2021-07-09 13:48:47 -0700

    fix: service name.

--- template/scripts/deploy.sh
+++ template/scripts/deploy.sh
@@ -4,4 +4,6 @@ set -e
 
 ./scripts/setup-kubectl-for-gcloud.sh
 
-helm upgrade --install --atomic springtest src/main/helm -n default -f src/main/helm/values.yaml --set config.image.tag="${TAG}"
+service_name="${PWD##*/}" #TODO: Quick hack, need to fix.
+
+helm upgrade --install --atomic "${service_name}" src/main/helm -n default -f src/main/helm/values.yaml --set config.image.tag="${TAG}"
